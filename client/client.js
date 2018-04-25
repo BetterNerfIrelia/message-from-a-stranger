2596c5e57a2532bdbcba06884cc035eb95898aea
Florian Hämmerle <haemmerle@betterthings.digital>
2018-04-25 15:56:32 +0200

    add client logic

--- client/client.js
+++ client/client.js
@@ -1,1 +1,41 @@
-// nothing here yet
+/** @type {HTMLButtonElement} */
+const submitButton = document.querySelector('#send-message-button')
+/** @type {HTMLInputElement} */
+const messageInput = document.querySelector('#message-input')
+
+/** @type {HTMLDivElement[]} */
+const moodIcons = Array.from(document.querySelectorAll('.mood'))
+
+/** @type {string} */
+let mood = null
+
+const MOOD_SELECTED_CLASS = 'mood--selected'
+
+/**
+ * @param {HTMLDivElement} moodElement
+ */
+function selectMood(moodElement) {
+  moodIcons.forEach(el => el.classList.remove(MOOD_SELECTED_CLASS))
+  mood = moodElement.textContent
+  moodElement.classList.add(MOOD_SELECTED_CLASS)
+}
+
+// select the first moodicon
+selectMood(moodIcons[0])
+
+moodIcons.forEach(moodElement => {
+  moodElement.addEventListener('click', () => selectMood(moodElement))
+})
+
+submitButton.addEventListener('click', async () => {
+  await fetch('/send-message', {
+    method: 'POST',
+    body: JSON.stringify({
+      message: messageInput.value,
+      mood,
+    }),
+  })
+  // reset
+  messageInput.value = ''
+  selectMood(moodIcons[0])
+})
